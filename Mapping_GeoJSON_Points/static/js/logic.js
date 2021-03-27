d36515867a8a3dcd1f6fa7fcd0f612179a123fce
gillranvir <74169932+gillranvir@users.noreply.github.com>
2021-03-27 13:25:20 -0400

    Mapping GoeJSON Points

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,50 +1,5 @@
-<<<<<<< Updated upstream
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
-
-// Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// Grabbing our GeoJSON data.
-L.geoJson(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  // pointToLayer: function(feature, latlng) {
-  onEachFeature: function(feature, layer) {  
-    console.log(layer);
-    layer.bindPopup();
-    // return L.marker(latlng)
-    // .bindPopup("<h2>" + feature.properties.city + "</h2>")
-  }
-
-}).addTo(map);
-
-
-// L.geoJson(data, {
-//   pointToLayer: function(feature, latlng) {
-//     return L.marker(latlng);
-//    }
-// });
-=======
 // Create the map object with center and zoom level.
 let map = L.map('mapid').setView([30, 30], 2.5);
->>>>>>> Stashed changes
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -53,11 +8,6 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-<<<<<<< Updated upstream
-
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
-=======
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
@@ -70,4 +20,3 @@ d3.json(airportData).then(function(data) {
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data).addTo(map);
 });
->>>>>>> Stashed changes
